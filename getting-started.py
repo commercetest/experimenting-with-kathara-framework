e92a86b266c4530369689141cf7c5fecea53bbdb
Julian Harty <julianharty@gmail.com>
2024-12-19 20:01:23 +0000

    This includes a router using the quagga image and nodes can connect over it.
    Apart from creating a second logical network 'B' and adding IP addresses no other
    configuration was needed. I did *NOT* need to start quagga on the router, it just worked.
    
    I'd expected to need to also call: `systemctl start quagga` in router1.startup hmmm.
    
    Still 3 python errors when the program stops. I'd like to address these ASAP.

--- getting-started.py
+++ getting-started.py
@@ -13,9 +13,15 @@ pc1 = lab.new_machine("pc1")
 pc2 = lab.new_machine("pc2")
 webserver = lab.new_machine("webserver")
 
+# Create router1 with image "kathara/frr"
+router1 = lab.new_machine("router1", **{"image": "kathara/quagga"})
+
+
 lab.connect_machine_to_link(pc1.name, "A")
 lab.connect_machine_to_link(pc2.name, "A")
-lab.connect_machine_to_link(webserver.name, "A")
+lab.connect_machine_to_link(webserver.name, "B")
+lab.connect_machine_to_link(router1.name, "A")
+lab.connect_machine_to_link(router1.name, "B")
 
 # NB: the following commands silently fail without the commas.
 # I think the last of the commands is processed as the web server does start apache2
@@ -38,11 +44,19 @@ lab.create_file_from_list(
 
 lab.create_file_from_list(
     [
-        "ip address add 100.1.2.90/24 dev eth0",
-        "ip route add default via 100.1.2.1 dev eth0",
+        "ip address add 100.1.3.90/24 dev eth0",
+        "ip route add default via 100.1.3.1 dev eth0",
         "systemctl start apache2"
     ],
     "webserver.startup"
+)
+
+lab.create_file_from_list(
+    [
+        "ip address add 100.1.2.1/24 dev eth0",
+        "ip address add 100.1.3.1/24 dev eth1",
+    ],
+    "router1.startup"
 )
 
 Kathara.get_instance().deploy_lab(lab)
@@ -52,9 +66,12 @@ print(lab_status)
 
 Kathara.get_instance().connect_tty(webserver.name, lab_name=lab.name)
 Kathara.get_instance().connect_tty(pc1.name, lab_name=lab.name)
-Kathara.get_instance().connect_tty(pc2.name, lab_name=lab.name)
+Kathara.get_instance().connect_tty(router1.name, lab_name=lab.name)
 
+print("Network A follows")
 print(Kathara.get_instance().get_link_stats(lab_name=lab.name, link_name="A"))
+print("Network B follows")
+print(Kathara.get_instance().get_link_stats(lab_name=lab.name, link_name="B"))
 
 # Currently there are 3 python errors reported, seemingly one for each connect_tty call.
 Kathara.get_instance().wipe()
